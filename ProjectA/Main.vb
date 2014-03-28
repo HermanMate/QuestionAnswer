64cf88f9d2b548a0cdbd3212e2a9a9affc1a9964
Philipp Erlacher <Philipp.Erlacher@advokat.at>
2014-03-28 09:51:30 +0100

    adds ProjectA and ProjectATest

---
+++ ProjectA/Main.vb
@@ -0,0 +1,15 @@
+﻿Public Class Main
+
+    ReadOnly Property Answer As Integer
+        Get
+            Return 42
+        End Get
+    End Property
+
+    ReadOnly Property Question As String
+        Get
+            Return "What do you get if you multiply six by nine?"
+        End Get
+    End Property
+
+End Class
---
+++ ProjectATest/MainTest.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class MainTest
+
+    <TestMethod()> Public Sub AnswerTest()
+        With New ProjectA.Main
+            Assert.AreEqual(42, .Answer)
+        End With
+    End Sub
+
+End Class
